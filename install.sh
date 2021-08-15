694dd6b595b5f5f98164e34369281dca1f4532b4
Thies Nieborg <thies2003@gmail.com>
2021-08-15 21:35:27 +0200

    Added gpg and docker

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
     exit 1
 fi
 
-OPTIONS=dzs:pgP
-LONGOPTS=debug,nozsh,ssh:,python,gui,personal
+OPTIONS=dzs:pgPDg:
+LONGOPTS=debug,nozsh,ssh:,python,gui,personal,docker,gpg:
 
 # -regarding ! and PIPESTATUS see above
 # -temporarily store output to be able to check for errors
@@ -31,7 +31,9 @@ if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
     echo "  -d, --debug                  Enable debug options (echo warnings)"
     echo "  -z, --nozsh                  Do no install zsh"
     echo "  -p, --python                 Install pyenv"
-    echo "  -s, --ssh [email]            Generate a github ssh key with provided email (Program WILL ask for a ssh password)"
+    echo "  -s, --ssh [key]              Import ssh key from specific file"
+    echo "  -g, --gpg [gpg key | y]      If Y, checks for imported gpg keys and adds them to git. With gpg filename it imports and then adds"
+    echo "  -D, --docker                 install docker & docker compose"
     echo "  -g, --gui                    Install gui apps (Vscode, Google Chrome)"
     echo "  -P, --personal               Install gui apps (Spotify, Discord, Whatsapp)"
 
@@ -42,6 +44,7 @@ eval set -- "$PARSED"
 
 d=y zsh=y pyth=n
 ssh=- gui=n guiextra=n
+docker=n gpg=-
 # now enjoy the options in order and nicely split until we see --
 while true; do
     case "$1" in
@@ -65,9 +68,17 @@ while true; do
             guiextra=y
             shift
             ;;
+        -g|--gpg)
+            gpg="$2"
+            shift 2
+            ;;
         -s|--ssh)
             ssh="$2"
             shift 2
+            ;;
+        -D|--docker)
+            docker=y
+            shift
             ;;
         --)
             shift
@@ -102,15 +113,30 @@ apt install zsh git net-tools curl software-properties-common apt-transport-http
 echo "Installed deps for all scripts and software"
 EOF
 
-# Install keychain https://www.cyberciti.biz/faq/ubuntu-debian-linux-server-install-keychain-apt-get-command/
 if [ $ssh != - ];
 then
     echo "Generating SSH key"
     sudo apt-get install keychain
-    ssh-keygen -t ed25519 -f $HOME/.ssh/id_github -C $ssh
-# Perhaps generate SSH & GPG keys
+    ssh-add $ssh
 fi
 
+
+
+if [ $gpg != - ];
+then
+    if [ $gpg != y ];
+    then
+        gpg --import $gpg
+    fi
+    key=$(gpg --list-secret-keys --keyid-format=long --with-colons | awk -F: '/sec:-:4096/ { print $5 }')
+    echo "Parsed key: $key"
+    git config --global user.signingkey $key
+    git config --global commit.gpgsign true
+
+ if [ -r ~/.bash_profile ]; then echo 'export GPG_TTY=$(tty)' >> ~/.bash_profile; \
+  else echo 'export GPG_TTY=$(tty)' >> ~/.profile; fi
+
+fi  
 
 if [ $zsh = y ];
 then
@@ -132,7 +158,7 @@ then
 
     if [ $ssh != - ];
     then
-        args+='/usr/bin/keychain --clear $HOME/.ssh/id_github \n source $HOME/.keychain/$HOSTNAME-sh'
+        args+='/usr/bin/keychain --clear $HOME/.ssh/'+$ssh+' \n source $HOME/.keychain/$HOSTNAME-sh'
     fi
 
     sh -c "$(wget -O- https://raw.githubusercontent.com/Thiesjoo/linuxinstall/main/assets/ohymyzsh.sh)" -- $args \
@@ -148,16 +174,13 @@ fi
 # Install GUI apps
 if [ $gui = y ];
 then
-    if ! command -v google-chrome &> /dev/null
-then
+    #Vscode
     wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
     sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
     debug && echo "Added vscode repo's"
-        #Vscode
     sudo apt install code
     echo "Installed vscode"
-    fi
-        # Chrome
+    # Chrome
     if ! command -v google-chrome &> /dev/null
     then
         cd /tmp
@@ -172,8 +195,6 @@ then
         
         echo "For further chrome tweaking: go to chrome://flags and disable hardware-media-key-handling. For 4k display set page zoom in chrome://settings to 150%"    fi
     fi
-
-
 
     if [ $guiextra = y ];
     then    
@@ -206,6 +227,21 @@ then
 fi
 
 
+
+if [ $docker = y ];
+then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg --batch --yes
+    echo \
+    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update
+    sudo apt install docker-ce docker-ce-cli containerd.io
+    # sudo groupadd docker 
+    sudo usermod -aG docker $USER
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    docker run hello-world
+fi
+
 # TODO: AUto github key upload? (You can just copy your old key)
 # Auto apply gnome config https://linuxconfig.org/how-to-install-gnome-shell-extensions-from-zip-file-using-command-line-on-ubuntu-18-04-bionic-beaver-linux
 # Maybe spicetify
@@ -217,4 +253,15 @@ echo "
     pyenv global 3.9.6
     nvm install 14 --lts"
 
+echo "To restore gnome config: dconf load / < saved_settings.dconf"
+echo "(Also copy extensions folders ~/.local/share/gnome-shell/extensions )"
+echo "Setup git creds:
+  git config --global user.email 'you@example.com'
+  git config --global user.name 'Your Name'
+"
+
+if [ $docker = y ];
+then
+    newgrp docker 
+fi
 zsh
